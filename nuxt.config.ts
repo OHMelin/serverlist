b512d8189abef19a9d5f508f2ef3150a64606a37
OHMelin <oliverholstmelin@gmail.com>
2025-12-18 15:43:37 +0100

    Favicon setup
    Fixes #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,5 +17,26 @@ export default defineNuxtConfig({
         external: [/^@opentelemetry\/.*/]
       }
     }
-  })
+  }),
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'keywords', content: 'Minecraft, Serverlist, Gaming, Community' },
+        { name: "apple-mobile-web-app-title", content: "Bookshelf" },
+        { name: 'author', content: 'Valoks' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ]
+    }
+  }
 })
