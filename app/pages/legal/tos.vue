e5a81ab16718d9ec204ee1cc0ed437fe7897fa19
OHMelin <oliverholstmelin@gmail.com>
2025-12-21 22:11:00 +0100

    Add ToS & Privacy Policy
    Fixes #41 & #42

--- app/pages/legal/tos.vue
+++ app/pages/legal/tos.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <p>Terms of Service page</p>
+    <UPageHero title="Terms of Service" />
+
+    <UPageSection :ui="{ container: '!pt-0' }">
+      <ContentRenderer :value="tos" />
+    </UPageSection>
   </div>
 </template>
+
+<script setup>
+const tos = await queryCollection('legal').path('/legal/tos').first()
+
+useHead(tos.head || {})
+useSeoMeta({
+  title: 'Terms of Service',
+})
+</script>
